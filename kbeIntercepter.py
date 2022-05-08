c9a8029ea40acc85b946a76f9ee1c123f32d90dd
Vawlpe <vawlpe@gmail.com>
2022-05-08 12:50:13 +0000

    Use evdev to block default behaviour using `grab/ungrab`

--- kbeIntercepter.py
+++ kbeIntercepter.py
@@ -1,34 +1,25 @@
 import os
-import struct
 import threading
+import evdev
 
 
-def RunShell(cmd):
-    os.system(f"{cmd} &")
+def RunShell(cmd, block):
+    os.system(f"{cmd}{'' if block == 'True' else ' &'}")
 
-# TODO: Intercept default key behaviour
 
 def EventCheckLoop(kbid, binds):
-    print(f"[{threading.current_thread().name}] Opening device file @ {kbid}")
-    FORMAT = 'llHHI'
-    EVENT_SIZE = struct.calcsize(FORMAT)
-    kb = open(kbid, "rb")
-    event = kb.read(EVENT_SIZE)
+    print(f"[{threading.current_thread().name}] Starting EventCheckLoop")
+    device = evdev.InputDevice(kbid)
 
-    print(f"[{threading.current_thread().name}] Starting event loop for {kbid}")
-    while event:
-        (tv_sec, tv_usec, eventType, code, value) = struct.unpack(FORMAT, event)
-        if (eventType, code, value) in [(0, 0, 0)]:
-            print(f"[{threading.current_thread().name}] ===========================================")
-            pass
-        elif (eventType, value) in [(1, 1)]:
-            print(f"[{threading.current_thread().name}] KeyDown: {code}")
-            if f"{code}" in binds:
-                print(f"[{threading.current_thread().name}] KeyCode {code} bound to {binds['%s' % code]}")
-                RunShell(binds[f"{code}"])
-        else:
-            print(f"[{threading.current_thread().name}] T:{eventType}, C:{code}, V:{value}")
-            pass
-        event = kb.read(EVENT_SIZE)
+    if "keepDefaultBehaviour" in binds and binds["keepDefaultBehaviour"] == "False":
+        device.grab()
 
-    kb.close()
+    for event in device.read_loop():
+        print(f"[{threading.current_thread().name}] T:{event.type}, C:{event.code}, V:{event.value}")
+        if (event.type, event.value) in [(evdev.ecodes.EV_KEY, 1)]:
+            if f"{event.code}" in binds:
+                print(f"[{threading.current_thread().name}] KeyCode {event.code} bound to {binds['%s' % event.code]}")
+                RunShell(binds[f"{event.code}"]["run"], binds[f"{event.code}"]["blockThread"])
+
+    if "keepDefaultBehaviour" in binds and binds["keepDefaultBehaviour"] == "False":
+        device.ungrab()
