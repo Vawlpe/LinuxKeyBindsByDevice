5c64d21a45fb47bb80d6182d3b6674cf67f95d64
Vawlpe <vawlpe@gmail.com>
2022-05-10 00:20:12 +0300

    Start implementing config wizard, very wip but need sleep

--- cfg.py
+++ cfg.py
@@ -1,6 +1,9 @@
 import json
 import os.path
 import threading
+
+import evdev
+
 import kbeIntercepter
 
 
@@ -19,5 +22,53 @@ def ReadCfg(path):
         print(f"[cfg.ReadCfg] Starting thread {t.name}")
         t.start()
 
-# TODO: WriteCfg func
-# TODO: CfgWizard func
+
+def WriteCfg(data, path):
+    json.dump(data, open(os.path.abspath(path)))
+
+
+def CfgWizard():
+    print("[CfgWizard] Starting config generation wizard")
+    print("[CfgWizard] Scanning for input devices...")
+    devices = [path for path in os.listdir("/dev/input/by-id")]
+    WizardAddDevice(devices)
+
+
+def WizardAddDevice(devices):
+    print("[CfgWizard] Found devices:")
+    for i in range(len(devices)):
+        print(f"\t{i}: {devices[i]}")
+
+    di = input(f"[CfgWizard] Choose a device to configure [0-{len(devices) - 1}], or exit (Q)): ")
+    if di.lower() == "q":
+        return
+
+    d = devices[int(di)]
+
+    WizardAddKeybind()
+
+def WizardAddKeybind(binds):
+    if len(binds) > 0:
+        print("Existing keybinds:")
+        for b in binds:
+            print(b)
+    else:
+        binds = {}
+
+    kcode = input("Keycode or exit (Q): ")
+    if kcode.lower() == "q":
+        return binds
+
+    rcmd = input("Command to run or exit (Q): ")
+    if rcmd.lower() == "q":
+        return binds
+
+    tblck = input("Should this command block the device read loop thread? (y/N)").lower() in ["yes", "y"]
+
+    #TODO: fix whatever this is
+    binds.append(kcode,
+        {
+            "run": rcmd,
+            "blockThread": tblck
+        }
+    })
--- main.py
+++ main.py
@@ -2,17 +2,48 @@
 
 import sys
 import cfg
+import os.path as path
 
 
 def main(args):
     print(f"[main.main] Args: {args}")
-    if len(args) < 1:
-        # TODO: if no args, or config doesn't exist, bring up cfg.CfgWizard
-        cfg.ReadCfg("~/.config/lkbbd.json")
+    if len(args) < 2:
+        execPath = path.abspath(path.dirname(args[0]))
+        print(f"Looking for config @ {path.expanduser('~/.config.lkbbd.json')}")
+        if path.exists(path.expanduser("~/.config/lkbbd.json")):
+            cfg.ReadCfg("~/.config/lkbbd.json")
+            return
+
+        print(f"Looking for config @ {path.join(execPath, 'lkbbd.json')}")
+        if path.exists(path.join(execPath, "lkbbd.json")):
+            cfg.ReadCfg(path.join(execPath, "lkbbd.json"))
+            return
+
+        # TODO: fix this shit
+        # if not extraPathCheck():
+        #     inp = input(f"Do you want to generate a new config @ {path.expanduser('~/.config/lkbbd.json')} (y/N): ")
+        #     if inp.lower() in ["yes", "y"]:
+        #         cfg.CfgWizard()
+        #     else:
+        #         print("Exiting, no config possible")
+        #         return
     else:
-        cfg.ReadCfg(args[0])
+        cfg.ReadCfg(args[1])
+
+
+def extraPathCheck():
+    inp = input("No config found, do you want to try a different path? (y/N): ")
+    if inp.lower() in ["yes", "y"]:
+        p = input("Input a custom config file path: ")
+        if path.exists(path.abspath(p)):
+            cfg.ReadCfg(path.abspath(p))
+            return True
+        else:
+            return extraPathCheck()
+    else:
+        return False
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main(sys.argv)
 
