c624d94708b2104c9572416368d2b3ec0ca5257d
Vawlpe <vawlpe@gmail.com>
2022-05-10 13:48:01 +0300

    Finished config wizard, hopefully no bug lol

--- cfg.py
+++ cfg.py
@@ -1,25 +1,22 @@
 import json
 import os.path
 import threading
-
-import evdev
-
 import kbeIntercepter
 
 
 def ReadCfg(path):
-    print(f"[cfg.ReadCfg] Loading from {path}")
+    print(f"[ReadCfg] Loading from {path}")
     data = json.load(open(os.path.expanduser(path)))
     threads = []
     for kbID, binds in data.items():
-        print(f"[cfg.ReadCfg] Creating thread for {kbID}")
+        print(f"[ReadCfg] Creating thread for {kbID}")
         threads.append(threading.Thread(
             target=kbeIntercepter.EventCheckLoop,
             args=(kbID, binds,)
         ))
 
     for t in threads:
-        print(f"[cfg.ReadCfg] Starting thread {t.name}")
+        print(f"[ReadCfg] Starting thread {t.name}")
         t.start()
 
 
@@ -31,10 +28,18 @@ def CfgWizard():
     print("[CfgWizard] Starting config generation wizard")
     print("[CfgWizard] Scanning for input devices...")
     devices = [path for path in os.listdir("/dev/input/by-id")]
-    WizardAddDevice(devices)
+    bindings = WizardAddDevice(devices)
+    if len(bindings) > 0:
+        print(f"\n----------------------------\n{bindings}\n----------------------------")
+        if input("[CfgWizard] Finishing up configuration, are you sure you want to keep this configuration? (y/N): ").lower() in ["yes", "y"]:
+            WriteCfg(bindings, os.path.expanduser('~/.config/lkbbd.json'))
+            input("[CfgWizard] Done, re-run lkbbd and it should detect the new configuration")
+        else:
+            print("[CfgWizard] DISCARDING CONFIG AND EXITING...")
+            return
 
 
-def WizardAddDevice(devices):
+def WizardAddDevice(devices, bindings={}):
     print("[CfgWizard] Found devices:")
     for i in range(len(devices)):
         print(f"\t{i}: {devices[i]}")
@@ -45,30 +50,54 @@ def WizardAddDevice(devices):
 
     d = devices[int(di)]
 
-    WizardAddKeybind()
-
-def WizardAddKeybind(binds):
+    binds = WizardAddKeybind()
     if len(binds) > 0:
-        print("Existing keybinds:")
-        for b in binds:
-            print(b)
-    else:
-        binds = {}
+        print(f"[CfgWizard] Device {di}: ({devices[int(di)]}) configured with the following bindings: ")
+        for k, b in binds.items():
+            print(f"\t{k} -> {b['run']}{' (blocking)' if b['blockThread'] == 'True' else ''}")
 
-    kcode = input("Keycode or exit (Q): ")
+        fwd = input(f"[CfgWizard] Do you wish to allow the default actions of keybinds for this device to still trigger? (y/N): ").lower() in ["yes", "y"]
+
+        binds.update({"keepDefaultBehaviour": f"{fwd}"})
+        bindings.update({os.path.join("/dev/input/by-id/", devices[int(di)]): binds})
+
+        if input(f"[CfgWizard] Bindings for device {di} have been configured, do you wish to configure another device? (y/N): ").lower() in ["yes", "y"]:
+            return WizardAddKeybind(devices, bindings)
+    else:
+        if input(f"[CfgWizard] There were no valid bindings configured for device {di}, do you wish to configure another device? (y/N): ").lower() in ["yes", "y"]:
+            return WizardAddKeybind(devices, bindings)
+
+    return bindings
+
+
+def WizardAddKeybind(binds={}):
+    if len(binds) > 0:
+        print("[CfgWizard] Existing keybinds:")
+        for b in binds:
+            print(f"\t{b}")
+
+    kcode = input("[CfgWizard] Keycode or exit (Q): ")
     if kcode.lower() == "q":
         return binds
 
-    rcmd = input("Command to run or exit (Q): ")
+    rcmd = input("[CfgWizard] Command to run or exit (Q): ")
     if rcmd.lower() == "q":
         return binds
 
-    tblck = input("Should this command block the device read loop thread? (y/N)").lower() in ["yes", "y"]
+    tblck = input("[CfgWizard] Should this keybind block the device read loop thread? (y/N)").lower() in ["yes", "y"]
 
-    #TODO: fix whatever this is
-    binds.append(kcode,
-        {
+    binds.update({
+        f"{kcode}": {
             "run": rcmd,
-            "blockThread": tblck
+            "blockThread": f"{tblck}"
         }
     })
+
+    print(f"[CfgWizard] Added Keybind for {kcode} -> {rcmd}{' (blocking)' if tblck else ''}")
+
+    if input("[CfgWizard] Do you want to add another keybind? (y/N): ").lower() in ["yes", "y"]:
+        return WizardAddKeybind(binds)
+
+    return binds
+
+
--- main.py
+++ main.py
@@ -6,35 +6,34 @@ import os.path as path
 
 
 def main(args):
-    print(f"[main.main] Args: {args}")
+    print(f"[PY-main] Args: {args}")
     if len(args) < 2:
         execPath = path.abspath(path.dirname(args[0]))
-        print(f"Looking for config @ {path.expanduser('~/.config.lkbbd.json')}")
+        print(f"[PY-main] Looking for config @ {path.expanduser('~/.config.lkbbd.json')}")
         if path.exists(path.expanduser("~/.config/lkbbd.json")):
             cfg.ReadCfg("~/.config/lkbbd.json")
             return
 
-        print(f"Looking for config @ {path.join(execPath, 'lkbbd.json')}")
+        print(f"[PY-main] Looking for config @ {path.join(execPath, 'lkbbd.json')}")
         if path.exists(path.join(execPath, "lkbbd.json")):
             cfg.ReadCfg(path.join(execPath, "lkbbd.json"))
             return
 
-        # TODO: fix this shit
-        # if not extraPathCheck():
-        #     inp = input(f"Do you want to generate a new config @ {path.expanduser('~/.config/lkbbd.json')} (y/N): ")
-        #     if inp.lower() in ["yes", "y"]:
-        #         cfg.CfgWizard()
-        #     else:
-        #         print("Exiting, no config possible")
-        #         return
+        if not extraPathCheck():
+            inp = input(f"[PY-main] Do you want to generate a new config @ {path.expanduser('~/.config/lkbbd.json')} (y/N): ")
+            if inp.lower() in ["yes", "y"]:
+                cfg.CfgWizard()
+            else:
+                print("[PY-main] Exiting, no config possible")
+                return
     else:
         cfg.ReadCfg(args[1])
 
 
 def extraPathCheck():
-    inp = input("No config found, do you want to try a different path? (y/N): ")
+    inp = input("[PY-main] No config found, do you want to try a different path? (y/N): ")
     if inp.lower() in ["yes", "y"]:
-        p = input("Input a custom config file path: ")
+        p = input("[PY-main] Input a custom config file path: ")
         if path.exists(path.abspath(p)):
             cfg.ReadCfg(path.abspath(p))
             return True
